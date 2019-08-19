87238bdd9386f8db291ff479c3eaf3c16c645a54
brandonetes <brandonsetes@gmail.com>
2019-08-18 20:20:30 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
-docker build -t brandonetes/multi-client:latest -t brandonetes/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t brandonetes/multi-server:latest -t brandonetes/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t brandonetes/multi-worker:latest -t brandonetes/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push brandonetes/multi-client:latest
-docker push brandonetes/multi-server:latest
-docker push brandonetes/multi-worker:latest
-docker push brandonetes/multi-client:$SHA
-docker push brandonetes/multi-server:$SHA
-docker push brandonetes/multi-worker:$SHA
+docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push stephengrider/multi-client:latest
+docker push stephengrider/multi-server:latest
+docker push stephengrider/multi-worker:latest
+
+docker push stephengrider/multi-client:$SHA
+docker push stephengrider/multi-server:$SHA
+docker push stephengrider/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=brandonetes/multi-server:$SHA
-kubectl set image deployments/client-deployment server=brandonetes/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=brandonetes/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
